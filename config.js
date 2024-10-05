d8853631ceba7c8ca416b72e3d513dd44b1e8596
Sean <seanghunkim1006@gmail.com>
2024-10-05 14:36:41 -0400

    db url change

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ function getDatabaseUri() {
   console.log(process.env.NODE_ENV)
   return (process.env.NODE_ENV === "test")
     ? "postgresql://username:password@localhost:5432/jobly_test"
-    : process.env.DATABASE_URL || "postgresql://postgres.udvbvkvwufomrcbfhonp:postgres@aws-0-us-east-1.pooler.supabase.com:6543/postgres?sslmode=require";
+    : process.env.DATABASE_URL || "postgresql://postgres.udvbvkvwufomrcbfhonp:hnroaK2CsqrZSJre@aws-0-us-east-1.pooler.supabase.com:6543/postgres?sslmode=require";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
